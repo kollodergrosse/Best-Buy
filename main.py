57971cd47a8a3f56ad95dc3bb41f7e8b1a9e8562
Alexander Kolle <alexanderkolle@AirvonAlexander.fritz.box>
2026-06-17 17:49:38 +0200

    created a menu

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
+from store import Store
+import products
+import sys
+
+def end_program():
+    sys.exit()
+
 
 def start():
-    pass
+    actions = {"1" : ["List all products in store", Store.get_all_products],
+               "2" : ["Show total amount in store", Store.get_total_quantity],
+               "3" : ["Make an order", Store.order],
+               "4" : ["Quit", end_program]}
+
+    print("   Store menu")
+    print("  ", 10 * "-")
+    for number, action in actions.items():
+        print(f"{number}. {action[0]}")
+
+    user_input = input("Please select an action: ")
+    actions[user_input][1]()
+
 
 def main():
-    pass
+
+    product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
+                    products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                    products.Product("Google Pixel 7", price=500, quantity=250)
+                    ]
+    #best_buy = Store(product_list)
+    start()
 
 if __name__ == '__main__':
     main()
--- store.py
+++ store.py
@@ -26,10 +26,3 @@ class Store:
         quantity = shopping_list[1]
         return products.Product.buy(article, quantity)
 
-
-bose = products.Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-mac = products.Product("MacBook Air M2", price=1450, quantity=100)
-
-best_buy = Store([bose, mac])
-price = best_buy.order([(bose, 5), (mac, 30), (bose, 10)])
-print(f"Order cost: {price} dollars.")
